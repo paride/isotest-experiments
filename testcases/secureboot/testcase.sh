1111b74ec9c5867be8313c415d10e1320fc11c6a
Paride Legovini <paride.legovini@canonical.com>
2019-12-05 15:45:40 +0100

    secureboot: fix for compatibility with virt-install 1.5

--- testcases/secureboot/testcase.sh
+++ testcases/secureboot/testcase.sh
@@ -3,7 +3,7 @@
 
 virt_install_opts() {
     # Recommended custom UEFI setup, see virt-install(1)
-    vi_opts_boot=(--boot loader=/usr/share/OVMF/OVMF_CODE.secboot.fd,loader.readonly=yes,loader.type=pflash,nvram.template=/usr/share/OVMF/OVMF_VARS.ms.fd,loader_secure=yes)
+    vi_opts_boot=(--boot loader=/usr/share/OVMF/OVMF_CODE.secboot.fd,loader_ro=yes,loader_type=pflash,nvram_template=/usr/share/OVMF/OVMF_VARS.ms.fd,loader_secure=yes)
     vi_opts_extra=(--features smm=on)
 }
 
